e807c8c79c05663dad26d9e8de6ff0a669145f2d
Rahul Daware <rdaware@cisco.com>
2018-10-15 16:59:07 -0700

    Added DB Checks while starting app

--- src/main/java/database/MariaDBConnector.java
+++ src/main/java/database/MariaDBConnector.java
@@ -8,8 +8,10 @@ import java.sql.Statement;
 public class MariaDBConnector {
 
     static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-    static final String DB_URL = "jdbc:mariadb://localhost:3306/reservations";
+    static final String MARIA_DB_URL = "jdbc:mariadb://localhost:3306";
+    static final String DB_NAME = "reservations";
 
+    static final String CREATE_USERS_TABLE = "CREATE TABLE IF NOT EXISTS users(username VARCHAR(20), password VARCHAR(20), signup_date DATE, PRIMARY KEY(username))";
     static final String USER = "rdaware";
     static final String PASS = "password";
 
@@ -24,11 +26,26 @@ public class MariaDBConnector {
         return statement;
     }
 
+    public void createTables() {
+        try {
+            statement.executeQuery(CREATE_USERS_TABLE);
+            System.out.println("Users Table verified");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public MariaDBConnector() {
         try {
-            connection = DriverManager.getConnection(DB_URL + "?user=" + USER+"&password=" + PASS);
-            System.out.println("Connection to DB successful");
+            connection = DriverManager.getConnection(MARIA_DB_URL + "?user=" + USER+"&password=" + PASS);
             statement = connection.createStatement();
+            int result = statement.executeUpdate("CREATE DATABASE IF NOT EXISTS " + DB_NAME);
+            if(result == 0) System.out.println("Database verified");
+            connection = DriverManager.getConnection(MARIA_DB_URL +"/" + DB_NAME +"?user=" + USER+"&password=" + PASS);
+            System.out.println("Connection to Database successful");
+            statement = connection.createStatement();
+            createTables();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
